e353bb2dd00fac7009bf97c7405dbd41bca806a3
Peter Winskill <p.winskill@imperial.ac.uk>
2025-09-08 13:07:28 +0100

    Testing with data

---
+++ Test_data_interpolation.R
@@ -0,0 +1,91 @@
+source("R/simulate.R")
+library(weave)
+library(progress)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(patchwork)
+library(sf)
+library(lubridate)
+
+
+sle_raw <- readRDS("data/SLE_routine_HF_malaria_outputs_with_chiefdom_centroids.rds")
+
+sle <- sle_raw |>
+  filter(variable == "Malaria treated with ACT <24 hours 0-59m_X") |>
+  mutate(
+    # Convert "month year" to a proper Date (first day of month)
+    ym = my(paste(month, year)),
+    # Find the first month across dataset
+    m = interval(min(ym), ym) %/% months(1) + 1,
+    lon = sapply(centroid, function(x){ x[2]}),
+    lat = sapply(centroid, function(x){ x[1]})
+  ) |>
+  mutate(t = m, n = cases) |>
+  select(name_2, name_3, HF, t, n, lat, lon) |>
+  sf::st_drop_geometry() |>
+  dplyr::mutate(
+    lat = lat + runif(1),
+    .by = c("name_2", "name_3", "HF")
+  ) |>
+  slice_head(n = 10000)
+
+obs_data <- weave:::data_process(sle, name_2, name_3, HF) |>
+  rename(y_obs = n) |>
+  dplyr::mutate(
+    z_infer = log1p(.data$y_obs),
+    mu_infer = mean(.data$z_infer, na.rm = TRUE),
+    f_infer = .data$z_infer - .data$mu_infer,
+    .by = id
+  )
+
+hf_labeller <- function(value) {
+  paste("HF:", value)
+}
+
+sim_plot <- ggplot() +
+  geom_point(data = obs_data, aes(x = t, y = y_obs), size = 1, colour = "black") +
+  facet_wrap(~ id, scales = "free_y", labeller = labeller(id = hf_labeller)) +
+  ylab("Cases") +
+  xlab("Month") +
+  theme_bw() +
+  theme(
+    legend.position = "none",
+    strip.background = element_rect(fill = "white", colour = "grey50"),
+    strip.text = element_text(size = 8, face = "bold"),
+    panel.spacing = unit(0.5, "lines")
+  ) +
+  ggtitle("Simulated data")
+
+# Infer kernel hyper-parameters ------------------------------------------------
+n <- length(levels(obs_data$id))
+nt <- max(obs_data$t)
+infer_space <- infer_space_kernel_params(obs_data, nt = nt, n = n, TRUE)
+infer_time <- infer_time_kernel_params(obs_data, 12, nt = nt, n = n, plot = TRUE)
+hyperparameters <- c(0.0000001, infer_time$periodic_scale / 2, infer_time$long_term_scale)
+# ------------------------------------------------------------------------------
+
+# Fit --------------------------------------------------------------------------
+coordinates <- unique(obs_data[, c("id", "lat", "lon")])
+system.time({
+  fit_data <- fit(obs_data, coordinates, hyperparameters, n, nt)
+})
+
+
+fit_plot <- sim_plot +
+  geom_vline(xintercept = 12 * 1:7, linetype = 2, col = "grey40") +
+  geom_ribbon(
+    data = fit_data,
+    aes(x = t, ymin = pred_Q2.5, ymax = pred_Q97.5, fill = id), alpha = 0.25
+  ) +
+  geom_ribbon(
+    data = fit_data,
+    aes(x = t, ymin = pred_Q25, ymax = pred_Q75, fill = id, alpha = 0.5)
+  ) +
+  geom_line(
+    data = fit_data,
+    aes(x = t, y = data_Q50, col = id), linewidth = 1
+  ) +
+  ggtitle("Filling missingness")
+fit_plot
+# ------------------------------------------------------------------------------
